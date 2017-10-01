62e0875c50803d3691fc4895936e1709c8a862be
Jakub Matuła <jakubmatula@imac.local>
2017-10-01 11:39:06 +0200

    Initial Commit

---
+++ JMWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JMWeatherApp
+//
+//  Created by Jakub Matuła on 01/10/2017.
+//  Copyright © 2017 Jakub Matuła. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
